f698d3da2fa6959aa1b44c749cb1ba2f651732b0
angelacaterina <angela.caterina@icloud.com>
2020-11-16 15:15:41 +0100

    js funzionante btn
    funzionano correttamente i <button>

--- assets/js/ticket.js
+++ assets/js/ticket.js
@@ -1,2 +1,14 @@
-var btnGenera = document.getElementById('id');
-var btnAnnulla = document.getElementById('id');
+var btnGenera = document.getElementById('btn_genara');
+var btnAnnulla = document.getElementById('btn_annulla');
+
+btnGenera.addEventListener('click',
+function(){
+  //Genara ticket
+  console.log('Genare Ticket');
+});
+
+btnAnnulla.addEventListener('click',
+function(){
+  //Annulla Ticket
+  console.log('Annulla operazione');
+});
